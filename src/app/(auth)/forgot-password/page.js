a4a9f8211b0b06dd4d2e104b9651aa6f4d5d969d
HienKoii <khien1892002@gmail.com>
2025-05-14 20:18:59 +0700

    thêm ForgotPasswordPage

---
+++ src/app/(auth)/forgot-password/page.js
@@ -0,0 +1,9 @@
+import Error from "@/components/error";
+
+export default function ForgotPasswordPage() {
+  return (
+    <div>
+      <Error text="Chức năng chưa hoàn thiện. Vui lòng quay lại sau !" />
+    </div>
+  );
+}
