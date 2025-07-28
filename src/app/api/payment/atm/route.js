64f09deb3060d70e2c3138398b4279ed618b738e
HienKoii <khien1892002@gmail.com>
2025-07-29 00:03:54 +0700

    f

--- src/app/api/payment/atm/route.js
+++ src/app/api/payment/atm/route.js
@@ -6,12 +6,11 @@ import db from "@/config/db";
 export async function POST(req) {
   try {
     const api = `https://api.sieuthicode.net/historyapivcbv2/${process.env.TOKEN_ATM}`;
-    console.log("api", api);
     const response = await fetch(api);
     const data = await response.json();
-    console.log("response", data);
+    console.log("response", data.transactions);
 
-    return NextResponse.json(data, { status: 200 });
+    return NextResponse.json(data.transactions, { status: 200 });
   } catch (error) {
     console.error("❌ Lỗi khi xử lý auto deposit:", error);
     return NextResponse.json({ message: "Lỗi xử lý auto deposit" }, { status: 500 });
